f83465c6fd2eda53284046134d4139122543bd3a
Dobrogost <dobrogost.adrian@gmail.com>
2023-06-19 21:15:56 +0200

    Poprawione zadanie

---
+++ Kolos_poprawa/Models/MyContext.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System.Security.Cryptography.Xml;
+
+namespace Kolos_poprawa.Models
+
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions options) : base(options)
+        {
+        }
+        //TUTAJ WSTAWIC WSZYSTKIE MODELE JAKO
+        // public DbSet<Client> Clients { get; set; }
+        public DbSet<Medicament> Medicaments { get; set; }
+        public DbSet<Prescription_Medicament> Prescription_Medicaments { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+
+        protected MyContext() { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Medicament>(e =>
+            {
+                e.HasData(new List<Medicament>
+                {
+                    new Medicament
+                    {
+                        IdMedicament = 1,
+                        Description = "desc",
+                        Name = "name",
+                        Type = "type"
+                    }
+            }); ;
+            });
+            modelBuilder.Entity<Prescription_Medicament>(e =>
+            {
+                e.HasData(new List<Prescription_Medicament>
+                {
+                    new Prescription_Medicament
+                    {
+                        IdMedicament = 1,
+                        IdPrescription = 1,
+                        Dose = 3,
+                        Details = "details"
+                    }
+            }); ;
+            });
+            modelBuilder.Entity<Prescription>(e =>
+            {
+                e.HasData(new List<Prescription>
+                {
+                    new Prescription
+                    {
+                        IdPrescription = 1,
+                        IdDoctor = 1,
+                        IdPatient = 1,
+                        Date = DateTime.Now,
+                        DueDate = DateTime.Now,
+                    }
+            }); ;
+            });
+            modelBuilder.Entity<Doctor>(e =>
+            {
+                e.HasData(new List<Doctor>
+                {
+                    new Doctor
+                    {
+                        IdDoctor = 1,
+                        FirstName = "Adrian",
+                        LastName = "Dylag",
+                        Email = "adylag@gmail.com"
+                    }
+            }); ;
+            });
+            modelBuilder.Entity<Patient>(e =>
+            {
+                e.HasData(new List<Patient>
+                {
+                    new Patient
+                    {
+                        IdPatient = 1,
+                        FirstName = "Janusz",
+                        LastName = "Tracz",
+                        Birthdate = DateTime.Now,
+                    }
+            }); ;
+            });
+        }
+    }
+}
--- Kolos_poprawa/Program.cs
+++ Kolos_poprawa/Program.cs
@@ -1,3 +1,7 @@
+using Kolos_poprawa.Models;
+using Microsoft.EntityFrameworkCore;
+using static Kolos_poprawa.Services.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IMyService, MyService>();
+builder.Services.AddDbContext<MyContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
@@ -15,8 +22,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
